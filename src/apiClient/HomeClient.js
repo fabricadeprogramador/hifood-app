203507bea2c13df37ad77cf0c86cc36ff6ca4f9d
Vinicius Souza <viniciuspodi@gmail.com>
2021-11-29 19:51:32 -0400

    Setando URL API final

--- src/apiClient/HomeClient.js
+++ src/apiClient/HomeClient.js
@@ -1,21 +1,26 @@
-const axios = require("axios");
-const API_HOST = "http://localhost:3000";
+const axios = require("axios")
+const API_HOST = "https://hifood-api.herokuapp.com/"
 
 export default {
+  listaProdutos() {
+    return axios
+      .get(API_HOST + "/produto")
+      .then(function (resposta) {
+        return resposta
+      })
+      .catch(function (error) {
+        return error
+      })
+  },
 
-    listaProdutos() {
-        return axios.get(API_HOST+"/produto").then(function(resposta) {
-            return resposta;
-        }).catch(function(error) {
-            return error;
-        });
-    },
-
-    listaCategorias() {
-        return axios.get(API_HOST+"/categoria").then(function(resposta) {
-            return resposta;
-        }).catch(function(error) {
-            return error;
-        });
-    }
+  listaCategorias() {
+    return axios
+      .get(API_HOST + "/categoria")
+      .then(function (resposta) {
+        return resposta
+      })
+      .catch(function (error) {
+        return error
+      })
+  }
 }
